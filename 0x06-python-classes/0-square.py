e26ec52f0bab5e2664373fcc615e93c42d675e0b
SamanSurafel17 <samanmobile168@gmail.com>
2022-06-21 10:53:57 -0700

    fix pystyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""
-square size
-"""
 
 
 class Square:
-    """Square class with
-    size.
-    """
 
     def __init__(self, size):
 
-        """initializes the size of variabl
-        """
         self.__size = size
