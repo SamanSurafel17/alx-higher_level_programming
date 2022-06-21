a87d8f2ede54719c19d9d139b74219f9da9d9c4f
SamanSurafel17 <samanmobile168@gmail.com>
2022-06-21 10:46:40 -0700

    fix square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+class Square:
+    def __init__(self, size):
+        
+        self.__size = size
