0a5878c181c96b3a2b47a2528dd76581e888a746
SamanSurafel17 <samanmobile168@gmail.com>
2022-11-06 08:24:40 -0800

    alx

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ends a request to that URL displays the size of the response body
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+# Get content length from request
+curl -sI "$1" |grep Content-Length|awk '{print $2}'
