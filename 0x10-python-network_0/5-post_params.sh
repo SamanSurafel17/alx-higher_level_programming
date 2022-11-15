bb8d7a17c2665dea909fe4d49caabb7e604658c9
SamanSurafel17 <samanmobile168@gmail.com>
2022-11-15 15:38:59 -0500

    network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script sends POST to URL
-curl -sL "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
-
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
