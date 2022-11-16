1fe8afcacc4e3982bc08985debc7e041b4e81e5b
SamanSurafel17 <samanmobile168@gmail.com>
2022-11-16 13:26:47 -0500

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-
-const url = process.argv[2]
-const request = require('request')
-
-request.get(url, function (error, response, body) {
-    if (!error) {
-        console.log('code:', response.statusCode)
-    }
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
